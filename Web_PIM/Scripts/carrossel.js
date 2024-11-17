a2315656c99b883fd7c75b9daa41e50f0bee6182
Murilo Gomes <murilojomes@gmail.com>
2024-11-17 02:24:17 -0300

    Murilo - "Sobre nós pronto para ser de fato feito" - 17/11

--- Web_PIM/Scripts/carrossel.js
+++ Web_PIM/Scripts/carrossel.js
@@ -1,10 +1,18 @@
-{
-  "exclude": [
-    "**/bin",
-    "**/bower_components",
-    "**/jspm_packages",
-    "**/node_modules",
-    "**/obj",
-    "**/platforms"
-  ]
+const box = document.querySelector(".slides");
+const imagens = document.querySelectorAll(".slides img");
+
+let contador = 0;
+
+function slider() {
+    contador++;
+
+    if (contador > imagens.length - 1) {
+        contador = 0;
+    }
+
+    box.style.transform = `translateX(${-contador * 800}px)`
+
 }
+
+setInterval( slider , 4000 )
+
